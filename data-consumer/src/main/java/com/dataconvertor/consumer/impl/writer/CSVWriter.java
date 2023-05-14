7b572f90e862202af8131dc08cfa4c00a26d6441
aditya-baranwal <baranwal.adi@gmail.com>
2023-05-14 18:39:23 +0530

    object based sheet data

--- data-consumer/src/main/java/com/dataconvertor/consumer/impl/writer/CSVWriter.java
+++ data-consumer/src/main/java/com/dataconvertor/consumer/impl/writer/CSVWriter.java
@@ -1,20 +1,39 @@
 package com.dataconvertor.consumer.impl.writer;
 
+import com.dataconvertor.common.enums.Operation;
 import com.dataconvertor.consumer.dao.OperationDao;
 import com.dataconvertor.consumer.interfaces.DataWriter;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
 
 @Component
 public class CSVWriter implements DataWriter {
+
+    private final CsvMapper csvMapper;
+
+    private final ObjectMapper jsonObjectMapper;
+
+    public CSVWriter() {
+        this.csvMapper = new CsvMapper();
+        this.csvMapper.configure(JsonGenerator.Feature.IGNORE_UNKNOWN, true);
+        this.jsonObjectMapper = new ObjectMapper();
+    }
 
     @Value("${spring.config.path.csv}")
     String filePath;
@@ -32,21 +51,36 @@ public class CSVWriter implements DataWriter {
             }
         }
 
-
         try (FileOutputStream fos = new FileOutputStream(filePath, true)) {
-            // mapper will map pojo according to json.
-            CsvMapper mapper = new CsvMapper();
-            mapper.configure(JsonGenerator.Feature.IGNORE_UNKNOWN, true);
+
+            JsonNode jsonObject = this.jsonObjectMapper.valueToTree(operationResult);
 
             // schema for csv file
-            CsvSchema schema = CsvSchema.builder().build();
+            CsvSchema.Builder builder = CsvSchema.builder().setUseHeader(true);
+            Iterator<Map.Entry<String, JsonNode>> fields = jsonObject.fields();
+            while (fields.hasNext()) {
+                Map.Entry<String, JsonNode> field = fields.next();
+                builder.addColumn(field.getKey());
+            }
+            CsvSchema schema = builder.build();
 
             // write to file
-            ObjectWriter writer = mapper.writerFor(OperationDao.class).with(schema);
+            ObjectWriter writer = this.csvMapper.writerFor(OperationDao.class).with(schema);
             writer.writeValues(fos).write(operationResult);
+
         } catch (IOException fe) {
             fe.printStackTrace();
             throw new RuntimeException(fe);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
--- data-consumer/src/main/java/com/dataconvertor/consumer/impl/writer/XLSXWriter.java
+++ data-consumer/src/main/java/com/dataconvertor/consumer/impl/writer/XLSXWriter.java
@@ -2,6 +2,10 @@ package com.dataconvertor.consumer.impl.writer;
 
 import com.dataconvertor.consumer.interfaces.DataWriter;
 import com.dataconvertor.consumer.dao.OperationDao;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -12,10 +16,18 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
 
 @Component
 public class XLSXWriter implements DataWriter {
 
+    private final ObjectMapper jsonObjectMapper;
+
+    public XLSXWriter() {
+        this.jsonObjectMapper = new ObjectMapper();
+    }
     @Value("${spring.config.path.xlsx}")
     String filePath;
 
@@ -27,16 +39,19 @@ public class XLSXWriter implements DataWriter {
             XSSFSheet sheet;
             if(!excelFile.exists())  {
                 wb = new XSSFWorkbook();
-                OutputStream fileOut = new FileOutputStream("operations.xlsx");
-                sheet = wb.createSheet("operation-output");
-                String[] headers = new String[] { "Number-1", "Number-2", "Operation", "Result" };
+//                OutputStream fileOut = new FileOutputStream("operations.xlsx");
+                OutputStream fileOut = new FileOutputStream(filePath);
+                sheet = wb.createSheet("sheet-1");
+                JsonNode jsonObject = this.jsonObjectMapper.valueToTree(operationResult);
+                Iterator<Map.Entry<String, JsonNode>> fields = jsonObject.fields();
                 Row r = sheet.createRow(0);
-                for (int cn=0; cn<headers.length; cn++) {
-                    r.createCell(cn).setCellValue(headers[cn]);
+                int i = 0;
+                while (fields.hasNext()) {
+                    Map.Entry<String, JsonNode> field = fields.next();
+                    r.createCell(i++).setCellValue(field.getKey());
                 }
                 wb.write(fileOut);
                 fileOut.close();
-                System.out.println("operation excel sheet not found, excel sheet has been created successfully");
             } else {
                 FileInputStream fileIn = new FileInputStream(excelFile);
                 wb = new XSSFWorkbook(fileIn);
